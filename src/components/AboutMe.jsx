c0a058c0f9d83a4723884d09ca9d47bf31cf29b4
unicorn453 <kira.ruseva@gmail.com>
2024-03-06 13:10:29 +0200

    added starter code

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        Hi, there, welcome to my portfolio page. My name is Kirila Ruseva, and I
+        am a Front-End Web Developer. I have always been passionate about
+        computer science, which is what led me to pursue a career in UX design.
+        I have gained extensive experience in Java Script ES6+, CSS3, HTML5,
+        SQL, React, Node, Query, and Bootstrap throughout my career. I have had
+        the opportunity to work with some amazing individuals and organisations.
+        I am proud to have been accredited with an edX certificate in Front-End
+        Development. I am always looking for ways to improve my skills further
+        and contribute to the success of my team and organisation. You can
+        usually find me in the library when I'm not working, where I like to
+        read history-related books and biographies. I also enjoy working out,
+        dancing, and spending time with family and friends. I believe it's
+        essential to maintain a healthy work-life balance and always make time
+        for the things that matter most in life.
+      </p>
+    </section>
+  );
+};
+
+export default AboutMe;
