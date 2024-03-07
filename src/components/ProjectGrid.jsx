0e321f5b8465873ac89437b206b43aebd9f09601
unicorn453 <kira.ruseva@gmail.com>
2024-03-07 20:59:19 +0200

    Added code for projects grid render

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import ProjectCard from "./ProjectCard";
 import projectsData from "../data/projectsData.json";
 
 const ProjectGrid = ({ projects, setActiveProject }) => {
+  const handleClick = (projectId) => {
+    setActiveProject(projectId);
+  };
+
   return (
     <div className="project-grid">
       {projects.map((project) => (
         <div
           key={project.id}
           className="project-grid-item"
-          onClick={() => setActiveProject(project.id)}
+          onClick={() => handleClick(project.id)}
         >
           <ProjectCard
             title={project.title}
             description={project.description}
             link={project.link}
+            github={project.github}
             image={project.image}
+            onClick={() => handleClick(project.id)}
           />
         </div>
       ))}
